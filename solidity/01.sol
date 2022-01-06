798d0349c2fcc5c5acd0ef1ab1f918767ef9b96d
rustamtrubnikov <trubnikov.r@list.ru>
2022-01-06 18:16:35 +0500

    upgrade

--- solidity/01.sol
+++ solidity/01.sol
@@ -2,22 +2,53 @@
 pragma solidity ^0.8.0;
 
 contract MyFirstShop {
+    mapping(address => bool) buyers;
     uint256 public price = 2 ether;
     address public owner;
     address public shopAddress;
-
+    bool fullyPaid = false;
     constructor()
     {
         owner = msg.sender;
         shopAddress = address(this);
     }
 
-    function getBalance() public view returns(uint)
-    {
+    event ItemFullyPaid(uint _price, address _shopAddress);
+    function getBalance() public view returns(uint) {
+
         return shopAddress.balance;
     }
 
     receive() external payable {
 
+        require(
+         owner == msg.sender
+         && msg.value <= price
+         && !fully  , "Reject");
+         if(shopAddress.balance == price) {
+            fullyPaid = true;
+
+            emit ItemFullyPaid(price,  shopAddress);
+        }
+    }
+
+    function getBayer(address _addr) public view returns(bool) {
+
+        require(owner == msg.sender, "You are not an owner!");
+        return  buyers[_addr];
+    }
+
+    function addBayer(address _addr) public {
+
+        require(owner == msg.sender, "You are not an owner!")
+        buyers[_addr] = true;
+    }
+
+    function withdrawAll() public {
+
+        require(owner == msg.sender && fullyPaid, "You are not an owner!");
+        address payable receiver = payable(msg.sender);
+
+        receiver.transfer(shopAddress.balance);
     }
 }
