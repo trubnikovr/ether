456fe557cab25d9de9755b7ee43b6a7d61c9bae7
rustamtrubnikov <trubnikov.r@list.ru>
2022-01-07 19:37:23 +0500

    update

---
+++ solidity/02.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Demo {
+    // numbers
+    int public myInt; // 0
+    uint8 public myInt; // 0
+
+    // bool
+    bool public myBoolValue;
+
+    // string
+    string myString; // memory | storage
+
+    // address
+    address myAddress;
+}
